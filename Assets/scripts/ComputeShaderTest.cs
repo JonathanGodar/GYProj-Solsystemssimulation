a559ff6400e4dfbae1b5ed41a267e59a2e8f1943
Jonathan Niklasson Godar <jonathan.godar@gmail.com>
2021-09-01 15:06:27 +0200

    HSLS testing

---
+++ Assets/scripts/ComputeShaderTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeShaderTest : MonoBehaviour
+{
+    public ComputeShader computeShader;
+
+    Cube[] cubes;
+
+    void Start()
+    {
+
+    }
+
+    struct Cube {
+        Vector3 pos;
+        Color color;
+    }
+
+    int count = 10;
+
+    [SerializeField] Mesh mesh;
+    [SerializeField] Material material;
+
+    private void CreateCube(int x, int y) {
+        GameObject cube = new GameObject("Cube " + x + y * count, typeof(MeshFilter), typeof(MeshCollider));
+        cube.GetComponent<MeshFilter>().mesh = mesh;
+        cube.GetComponent<MeshCollider>().material = material;
+
+        Color color = Random.ColorHSV();
+        cube.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
+
+
+    
+    }
+
+    private void RandomizeOnGPU() {
+
+
+
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        //if (renderTexture == null) {
+        //    renderTexture = new RenderTexture(256, 256, 24);
+        //    renderTexture.enableRandomWrite = true;
+        //    renderTexture.Create();
+        //}
+
+        //computeShader.SetTexture(0, "Result", renderTexture);
+        //computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        //Graphics.Blit(renderTexture, destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/Simulation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
