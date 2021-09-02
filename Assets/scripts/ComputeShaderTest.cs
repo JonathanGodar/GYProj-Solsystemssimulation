531e3b0adf72ce3725a4d84b930a9234150cd339
SilvrHorison <jonathan.godar@gmail.com>
2021-09-02 08:41:09 +0200

    Basic compute shader working

--- Assets/scripts/ComputeShaderTest.cs
+++ Assets/scripts/ComputeShaderTest.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ComputeShaderTest : MonoBehaviour
 {
@@ -10,53 +13,100 @@ public class ComputeShaderTest : MonoBehaviour
 
     void Start()
     {
-
+        GenerateCubes();
     }
 
     struct Cube {
-        Vector3 pos;
-        Color color;
+        public Vector3 pos;
+        public Color color;
     }
 
-    int count = 10;
+    const int count = 24;
 
     [SerializeField] Mesh mesh;
     [SerializeField] Material material;
 
+    List<GameObject> objects = new List<GameObject>();
+
+    Cube[] data = new Cube[count * count];
+
     private void CreateCube(int x, int y) {
-        GameObject cube = new GameObject("Cube " + x + y * count, typeof(MeshFilter), typeof(MeshCollider));
+        GameObject cube = new GameObject("Cube " + x + y * count, typeof(MeshFilter), typeof(MeshRenderer));
         cube.GetComponent<MeshFilter>().mesh = mesh;
-        cube.GetComponent<MeshCollider>().material = material;
+        cube.GetComponent<MeshRenderer>().material = new Material(material);
+
+        cube.transform.position = new Vector3(x, y, Random.Range(-0.1f, 0.1f));
+
 
         Color color = Random.ColorHSV();
         cube.GetComponent<MeshRenderer>().material.SetColor("_Color", color);
 
+        Cube cubeData = new Cube();
 
-    
+        objects.Add(cube);
+
+        cubeData.pos = cube.transform.position;
+        cubeData.color = color;
+
+        data[x + y * count] = cubeData;
+    }
+
+    private void GenerateCubes() {
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++)
+            {
+                CreateCube(i, j);
+                Debug.Log("Created cube");
+            }
+        }
+    }
+
+    private void RandomizeOnCpu() {
+        for (int c = 0; c < objects.Count; c++) {
+            GameObject obj = objects[c];
+            obj.transform.position = new Vector3(obj.transform.position.x, obj.transform.position.y, Random.Range(-0.1f, 0.1f));
+            obj.GetComponent<MeshRenderer>().material.SetColor("_Color", Random.ColorHSV());
+        }
     }
 
     private void RandomizeOnGPU() {
+        int colorSize = sizeof(float) * 4;
+        int positionSize = sizeof(float) * 3;
+
+        int totalSize = colorSize + positionSize;
+
+        ComputeBuffer cubesBuffer = new ComputeBuffer(data.Length, totalSize );
+        cubesBuffer.SetData(data);
+
+        computeShader.SetBuffer(0, "cubes", cubesBuffer);
+        computeShader.SetFloat("resolution", data.Length);
+
+        computeShader.Dispatch(0, data.Length / 10, 1, 1);
+
+        cubesBuffer.GetData(data);
+
+        for (int i = 0; i < objects.Count; i++) {
+            GameObject obj = objects[i];
+            Cube cube = data[i];
+            obj.transform.position = cube.pos;
+            obj.GetComponent<MeshRenderer>().material.SetColor("_Color", cube.color);
+        }
 
 
+        cubesBuffer.Dispose();
 
-    }
 
-    private void OnRenderImage(RenderTexture source, RenderTexture destination)
-    {
-        //if (renderTexture == null) {
-        //    renderTexture = new RenderTexture(256, 256, 24);
-        //    renderTexture.enableRandomWrite = true;
-        //    renderTexture.Create();
-        //}
-
-        //computeShader.SetTexture(0, "Result", renderTexture);
-        //computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
-        //Graphics.Blit(renderTexture, destination);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            RandomizeOnCpu();
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)) {
+            RandomizeOnGPU();
+        }
     }
 }
--- Assets/scripts/Simulation.cs
+++ Assets/scripts/Simulation.cs
@@ -1,23 +1,165 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Debug = UnityEngine.Debug;
 
 public class Simulation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] ComputeShader computeShader;
+    
+    const int PLANET_COUNT = 50_000;
+
+    Planet[] planets = new Planet[PLANET_COUNT];
+    ComputeBuffer planetBuffer;
+
+    GameObject[] objects = new GameObject[PLANET_COUNT];
+
+
+    int currPlanetId = 0;
+
+
+    [SerializeField] Mesh mesh;
+    [SerializeField] Material material;
+
+    int _planetsId;
+    int _planetCountId;
+
+    int simulationKernel = 0;
+
+    private void OnEnable()
     {
-        
+        _planetsId = Shader.PropertyToID("planets");
+        _planetCountId = Shader.PropertyToID("planetCount");
+
+        planetBuffer = new ComputeBuffer(PLANET_COUNT, PLANET_STRUCT_SIZE);
+        computeShader.SetBuffer(simulationKernel, _planetsId, planetBuffer);
+        computeShader.SetInt(_planetCountId, PLANET_COUNT);
+        GeneratePlanets();
     }
+
+    // 3 Vector3's + 2 floats + 1 byte ( + 3 because has to be multiple )
+    const int PLANET_STRUCT_SIZE = sizeof(float) * 3 * 3 + sizeof(float) * 2;
+    struct Planet
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+
+        public Vector3 force;
+
+        public float radius;
+        public float mass;
+    };
+
+    void GeneratePlanets(bool dataOnly = false)
+    {
+        if (dataOnly)
+        {
+            for (int i = 0; i < PLANET_COUNT; i++)
+            {
+               CreatePlanetData(
+                    currPlanetId++,
+                    new Vector3(
+                        Random.Range(-15, 15),
+                        Random.Range(-15, 15),
+                        Random.Range(-15, 15)
+                        )
+                    );
+            }
+        }
+        else
+        {
+            for (int i = 0; i < PLANET_COUNT; i++)
+            {
+                CreatePlanet(
+                    currPlanetId++,
+                    new Vector3(
+                        Random.Range(-5, 5),
+                        Random.Range(-5, 5),
+                        Random.Range(-5, 5)
+                        )
+                    );
+            }
+        }
+
+    }
+
+    void SyncPlanetsFromGpu()
+    {
+        planetBuffer.GetData(planets);
+
+        for (int i = 0; i < 100; i++)
+        {
+            // TODO update planet mass and radius
+            GameObject p = objects[i];
+            p.transform.position = planets[i].position;
+        }
+    }
+
+    void SyncPlanetsToGpu() {
+        planetBuffer.SetData(planets);
+    }
+
+
+
+    private void CreatePlanet(int id, Vector3 position, Vector3 velocity = new Vector3(), Vector3 force = new Vector3(), float radius = 1, float mass = 1)
+    {
+        CreatePlanetData(id, position, velocity, force, radius, mass);
+        CreatePlanetObject(id, position, radius);
+    }
+
+    private void CreatePlanetData(int id, Vector3 position, Vector3 velocity = new Vector3(), Vector3 force = new Vector3(), float radius = 1, float mass = 1)
+    {
+        Planet planetData = new Planet();
+
+        planetData.position = position;
+        planetData.velocity = id == 0 ? new Vector3(0, 1000, 0) : velocity;
+        planetData.radius = radius;
+        planetData.mass = mass;
+        planetData.force = force;
+        planets[id] = planetData;
+    }
+
+    private void CreatePlanetObject(int id, Vector3 position, float radius)
+    {
+
+        if (id >= 100)
+            return;
+
+        GameObject obj = new GameObject("Planet " + id, typeof(MeshFilter), typeof(MeshRenderer));
+        obj.GetComponent<MeshFilter>().mesh = mesh;
+        obj.GetComponent<MeshRenderer>().material = material;
+
+
+        obj.transform.localScale = Vector3.one * radius;
+        obj.transform.position = position;
+
+        objects[id] = obj;
+    }
+
 
     private void OnDisable()
     {
-        
+        planetBuffer.Dispose();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Stopwatch sw = new Stopwatch();
+
+            SyncPlanetsToGpu();
+            
+            sw.Start();
+            computeShader.Dispatch(0, Mathf.CeilToInt(PLANET_COUNT / 64f), 1, 1);
+            SyncPlanetsFromGpu();
+            sw.Stop();
+
+            Debug.Log(sw.ElapsedMilliseconds); 
+        }
     }
 }
